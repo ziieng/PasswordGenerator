55825a280d4087fe166ee5a9d73a6ad91cac9396
ziieng <cjengelhardt@gmail.com>
2020-09-18 18:17:40 -0700

    Initial push of files given in assignment

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////
+// PASSWORD GENERATOR
+//
+// * For this assignment, you will not be changing the HTML and CSS at all.
+//
+// * You will need a generatePassword function is called when the user
+//   clicks the Generate Password button.
+//
+// * You can create other functions that are called from within
+//   generatePassword
+//
+// * Gather user input with prompt's and confirm's
+
+///////////////////////////////////////////////////////////////////////
+// DO NOT TOUCH THIS CODE
+//
+// This code handles:
+// * clicking the Generate Password
+// * writing the password to the screen
+//
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//////////////////////////////////////////////////////////////////////
