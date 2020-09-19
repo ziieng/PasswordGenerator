9cbd7b3867bb78cbc071b4c1428280eb74c72087
ziieng <cjengelhardt@gmail.com>
2020-09-18 18:50:56 -0700

    pseudocode for js, random number function to resuse

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,7 @@
 // * You can create other functions that are called from within
 //   generatePassword
 //
-// * Gather user input with prompt's and confirm's
+// * Gather user input with prompts and confirms
 
 ///////////////////////////////////////////////////////////////////////
 // DO NOT TOUCH THIS CODE
@@ -33,3 +33,25 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //////////////////////////////////////////////////////////////////////
+
+//Function for random numbers recycled from class activity; assume min of 1, so removed it from parameters
+function getRandom(max) {
+  var min = 1
+  max = math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//Establish modified variables - character string to draw from, password as it's built
+//Prompt user for length (declare var in it)
+//Verify length is between 8 and 128
+
+//Ask user if they want lowercase, add to deck if yes
+//Ask user if they want uppercase, add to deck if yes
+//Ask user if they want numbers, add to deck if yes
+//Ask user if they want special characters, add to deck if yes
+//Verify at least 1 type of characters selected (loop while deck empty?)
+
+//Generate password with (length) characters randomly drawn from (deck)
+//Verify all requested character types are present?
+
+//Alert done, write password to page
